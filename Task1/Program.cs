463dbedb08f6349b4725f129600c2a5f5eb3bb59
Vitoz <vitozb@gmail.com>
2023-05-29 17:43:54 +0600

    Решена первая задача

---
+++ Task1/Program.cs
@@ -0,0 +1,4 @@
+﻿System.Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(System.Console.ReadLine());
+int result = (num % 100 - num % 10) / 10;
+System.Console.WriteLine("Второе число равно "+result);
