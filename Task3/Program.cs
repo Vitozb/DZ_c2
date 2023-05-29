4a6918f9538a9291e919b744026fc0669a74d61d
Vitoz <vitozb@gmail.com>
2023-05-29 19:21:30 +0600

    Решил 3 задачу

---
+++ Task3/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.Write("Введите цифру обозначающую день недели: ");
+int num = Convert.ToInt32(System.Console.ReadLine());
+if (num > 0 & num <=7)
+{
+    if (num == 6) System.Console.WriteLine("Отлично это Суббота");
+    if (num == 7) System.Console.WriteLine("Отлично это Воскресенье");
+    if (num > 0 & num < 6) System.Console.WriteLine("Это рабочий день");
+}
+else 
+    System.Console.WriteLine("Вы ввели неверное число");
