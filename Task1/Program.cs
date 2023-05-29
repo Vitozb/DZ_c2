39877cd9f41e698eb23054b39cebc9c543f0020c
Vitoz <vitozb@gmail.com>
2023-05-29 19:39:51 +0600

    Немного поправил первую задачу

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,4 +1,9 @@
 ﻿System.Console.Write("Введите трехзначное число: ");
 int num = Convert.ToInt32(System.Console.ReadLine());
+
+if (num >= 100 & num < 1000)
+{
 int result = (num % 100 - num % 10) / 10;
 System.Console.WriteLine("Второе число равно "+result);
+}
+else System.Console.WriteLine("Ввели некоректное число");
